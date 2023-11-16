67c5937b0a467a8099320d41d00d110da2b4cca1
Ophelie Arnoult <ophelie.arnoult250@gmail.com>
2023-11-16 16:00:21 +0100

    front fontaines

--- src/component/Fontaines/Fontaines.js
+++ src/component/Fontaines/Fontaines.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getFontainesFiltre } from '../../utilities/utils';
 import api from '../../API/api';
+import { Table } from 'react-bootstrap';
 
 function Fontaines() {
 
@@ -9,15 +10,11 @@ function Fontaines() {
     const [isMounted, setIsMounted] = useState(false);
     const [fontaines, setFontaines] = useState([]);
 
-    // api.getFontaines('refine=dispo:NON').then((json) => {
-    //     console.log('test', json)
-    // })
-
-
+    console.log('fontaines', fontaines)
 
     useEffect(() => {
         api.getFontainesExport().then((fontaines) => {
-            console.log('test', fontaines)
+            // console.log('test', fontaines)
 
             var filtreCommune = []
             fontaines.forEach(fontaine => {
@@ -54,13 +51,64 @@ function Fontaines() {
     }
 
 
+    console.log('fontaines1', fontaines)
 
     return (
         <div>
-            <p>Commune</p>
+            <div
+                style={{
+                    backgroundColor: '#5f259f',
+                    width: '100%',
+                    height: '30vh',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+
+                }}>
+                <img
+                    src={require('../../assets/image/Fontaines.JPG')}
+                    height={160}
+                    style={{
+                        alignSelf: 'center',
+                        borderRadius: 17,
+                        marginLeft: 30,
+                    }}
+                />
+                <div style={{ textAlign: 'end', alignSelf: 'center', paddingRight: 30 }}>
+                    <p style={{ margin: 0, fontSize: 28, color: 'white' }}>Fontaines à boire</p>
+                    <p style={{ margin: 0, fontSize: 25, fontFamily: 'nexaLight', color: 'white' }}>{fontaines.total_count + ' fontaines trouvées'}</p>
+                </div>
+            </div>
+            {/* <p>Commune</p>
             {filtres.map(filtre => <p onClick={() => {
                 _filtreFontaine('refine', 'commune:"' + filtre + '"')
-            }}>{filtre}</p>)}
+            }}>{filtre}</p>)} */}
+
+            <Table
+                style={{
+                    backgroundColor: 'rgba(95, 37, 159, 0.35)',
+                    margin: 40,
+                    width: '95%',
+                    borderRadius: 15,
+                    padding: 10,
+                    textAlign: 'center',
+                }}
+            >
+                <thead>
+                    <tr>
+                        <th>Adresse</th>
+                        <th>Commune</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fontaines.results.map((fontaine) =>
+                        <tr>
+                            <td>{((fontaine.no_voirie_pair === null ? fontaine.no_voirie_impair : fontaine.no_voirie_pair) || (fontaine.no_voirie_pair === null && fontaine.no_voirie_impair === null ? '' : <></>)) + ' ' + fontaine.voie}</td>
+                            <td>{fontaine.commune}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
         </div>
     );
 }
